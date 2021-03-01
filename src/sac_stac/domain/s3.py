21914a35e1d080f6353c3db84061973990c39c12
juan.macias <Juan.Macias@sa.catapult.org.uk>
2021-03-01 18:21:33 +0000

    Add s3 class

---
+++ src/sac_stac/domain/s3.py
@@ -0,0 +1,90 @@
+import logging
+
+import boto3
+from botocore.exceptions import ClientError
+from src.sac_stac.load_config import LOG_LEVEL, LOG_FORMAT
+
+logging.basicConfig(level=LOG_LEVEL, format=LOG_FORMAT)
+
+logger = logging.getLogger(__name__)
+
+
+class S3:
+    """Class to handle S3 operations."""
+
+    def __init__(self, key, secret, s3_endpoint, region_name):
+        """
+        Initialize s3 class.
+        Params:
+            key           (str): AWS_ACCESS_KEY_ID
+            secret        (str): AWS_SECRET_ACCESS_KEY
+            s3_endpoint   (str): S3 endpoint URL
+            region_name   (str): Region Name
+        """
+        self.s3_resource = boto3.resource(
+            "s3",
+            endpoint_url=s3_endpoint,
+            verify=False,
+            region_name=region_name,
+            aws_access_key_id=key,
+            aws_secret_access_key=secret,
+        )
+        self.buckets_exist = []
+
+    def list_objects(self, bucket_name, *, prefix=None, suffix=None, limit=None):
+        """
+        List objects stored in a bucket.
+        Params:
+            bucket_name      (str): Bucket name
+        Keyword arguments (opt):
+            prefix           (str): Filter only objects with specific prefix
+                                    default None
+            suffix           (str): Filter only objects with specific suffix
+                                    default None
+            limit            (int): Limit the number of objects returned
+                                    default None
+        Returns:
+            An iterable of ObjectSummary resources
+        """
+        if prefix:
+            objects = list(self.s3_resource.Bucket(bucket_name).objects.filter(Prefix=prefix).limit(limit))
+        else:
+            objects = list(self.s3_resource.Bucket(bucket_name).objects.all().limit(limit))
+
+        if suffix:
+            return [obj for obj in objects if obj.key.endswith(suffix)]
+        else:
+            return objects
+
+    def get_object_body(self, bucket_name, object_name):
+        """
+        Download an object from S3 and return its body.
+        Params:
+            bucket_name            (str): Bucket name
+            object_name            (str): Object name
+        """
+        try:
+            obj = self.s3_resource.Object(bucket_name=bucket_name, key=object_name).get()
+            return obj.get('Body').read()
+        except ClientError as ex:
+            if ex.response['Error']['Code'] == 'NoSuchKey':
+                logger.warning(f"No object found for {object_name} in {bucket_name} bucket")
+                return None
+            else:
+                raise
+
+    def list_common_prefixes(self, bucket_name, prefix):
+        """
+        List all common prefixes with the given prefix delimited by '/'.
+        Params:
+            bucket_name            (str): Bucket name
+            prefix                 (str): Prefix
+        """
+        common_prefixes = []
+        paginator = self.s3_resource.meta.client.get_paginator('list_objects')
+
+        for result in paginator.paginate(Bucket=bucket_name, Prefix=prefix, Delimiter='/'):
+            if result.get('CommonPrefixes'):
+                common_prefixes = [p.get('Prefix') for p in result.get('CommonPrefixes')]
+
+        return common_prefixes
---
+++ tests/unit/test_s3.py
@@ -0,0 +1,65 @@
+from pathlib import Path
+
+from moto import mock_s3
+from src.sac_stac.domain.s3 import S3
+
+
+BUCKET = 'test'
+
+
+def initialise_bucket(s3_resource, bucket_name):
+    s3_resource.create_bucket(Bucket=bucket_name)
+    for file in Path('tests/data/sentinel_2').glob('**/*.tif'):
+        s3_resource.Bucket(bucket_name).upload_file(
+            Filename=str(file),
+            Key=f"common_sensing/fiji/sentinel_2/{file.parent.stem}/{file.name}"
+        )
+
+
+@mock_s3
+def test_get_object_body():
+    s3 = S3(key=None, secret=None, s3_endpoint=None, region_name='us-east-1')
+    initialise_bucket(s3_resource=s3.s3_resource, bucket_name=BUCKET)
+
+    obj = s3.get_object_body(bucket_name=BUCKET,
+                             object_name='common_sensing/fiji/sentinel_2/'
+                                         'S2A_MSIL2A_20151022T222102_T01KBU/'
+                                         'S2A_MSIL2A_20151022T222102_T01KBU_AOT_10m.tif')
+
+    assert obj
+
+
+@mock_s3
+def test_get_object_body_non_exist():
+    s3 = S3(key=None, secret=None, s3_endpoint=None, region_name='us-east-1')
+    initialise_bucket(s3_resource=s3.s3_resource, bucket_name=BUCKET)
+
+    obj = s3.get_object_body(bucket_name=BUCKET,
+                             object_name='nothing')
+
+    assert not obj
+
+
+@mock_s3
+def test_list_common_prefixes():
+
+    s3 = S3(key=None, secret=None, s3_endpoint=None, region_name='us-east-1')
+    initialise_bucket(s3_resource=s3.s3_resource, bucket_name=BUCKET)
+
+    objs = s3.list_common_prefixes(bucket_name=BUCKET, prefix='common_sensing/fiji/sentinel_2/')
+
+    assert objs == ['common_sensing/fiji/sentinel_2/S2A_MSIL2A_20151022T222102_T01KBU/',
+                    'common_sensing/fiji/sentinel_2/S2B_MSIL2A_20191023T220919_T01KBA/',
+                    'common_sensing/fiji/sentinel_2/S2B_MSIL2A_20191023T220919_T01KBB/']
+
+
+@mock_s3
+def test_list_common_prefixes_not_exist():
+
+    s3 = S3(key=None, secret=None, s3_endpoint=None, region_name='us-east-1')
+    initialise_bucket(s3_resource=s3.s3_resource, bucket_name=BUCKET)
+
+    objs = s3.list_common_prefixes(bucket_name=BUCKET, prefix='common_sensing/fiji/sentinel_3/')
+
+    assert not objs
+
