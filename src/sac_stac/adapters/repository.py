b7dcb1f4a5ae9f0feb31cd02a2cd3e41f6e6ee72
juan.macias <Juan.Macias@sa.catapult.org.uk>
2021-03-02 10:33:44 +0000

    Refactor repository funcion names

--- src/sac_stac/adapters/repository.py
+++ src/sac_stac/adapters/repository.py
@@ -9,14 +9,15 @@ class S3Repository:
     def __init__(self, s3: S3):
         self.s3 = s3
 
-    def get_datasets(self, bucket: str, sensor_prefix: str) -> List[str]:
+    def get_acquisitions_from_sensor(self, bucket: str, sensor_prefix: str) -> List[str]:
         return self.s3.list_common_prefixes(bucket_name=bucket, prefix=sensor_prefix)
 
-    def get_measurements(self, bucket: str, dataset_prefix: str) -> List[str]:
-        return [Path(o.key).stem for o in self.s3.list_objects(bucket_name=bucket, prefix=dataset_prefix, suffix='.tif')]
+    def get_products_from_acquisition(self, bucket: str, acquisitions_prefix: str) -> List[str]:
+        product_objs = self.s3.list_objects(bucket_name=bucket, prefix=acquisitions_prefix, suffix='.tif')
+        return [Path(p.key).stem for p in product_objs]
 
-    def get_smallest_measurement_sample(self, bucket: str, dataset_prefix: str) -> bytes:
-        measurements = self.s3.list_objects(bucket_name=bucket, prefix=dataset_prefix, suffix='.tif')
-        measurements_size = {m.size: m.key for m in measurements if m.size > 1}
-        min_size = min(list(measurements_size.keys()))
-        return self.s3.get_object_body(bucket_name=bucket, object_name=measurements_size.get(min_size))
+    def get_smallest_product_sample(self, bucket: str, acquisitions_prefix: str) -> bytes:
+        product_objs = self.s3.list_objects(bucket_name=bucket, prefix=acquisitions_prefix, suffix='.tif')
+        product_objs_size = {p.size: p.key for p in product_objs if p.size > 1}
+        product_min_size = min(list(product_objs_size.keys()))
+        return self.s3.get_object_body(bucket_name=bucket, object_name=product_objs_size.get(product_min_size))
--- tests/integration/test_repository.py
+++ tests/integration/test_repository.py
@@ -16,46 +16,47 @@ def initialise_cs_bucket(s3_resource, bucket_name):
 
 
 @mock_s3
-def test_get_datasets():
+def test_get_acquisitions_from_sensor():
     s3 = S3(key=None, secret=None, s3_endpoint=None, region_name='us-east-1')
     initialise_cs_bucket(s3_resource=s3.s3_resource, bucket_name=BUCKET)
 
     repo = repository.S3Repository(s3)
-    datasets = repo.get_datasets(bucket=BUCKET, sensor_prefix='common_sensing/fiji/sentinel_2/')
+    acquisitions = repo.get_acquisitions_from_sensor(bucket=BUCKET, sensor_prefix='common_sensing/fiji/sentinel_2/')
 
-    assert datasets == ['common_sensing/fiji/sentinel_2/S2A_MSIL2A_20151022T222102_T01KBU/',
-                        'common_sensing/fiji/sentinel_2/S2B_MSIL2A_20191023T220919_T01KBA/',
-                        'common_sensing/fiji/sentinel_2/S2B_MSIL2A_20191023T220919_T01KBB/']
+    assert acquisitions == ['common_sensing/fiji/sentinel_2/S2A_MSIL2A_20151022T222102_T01KBU/',
+                            'common_sensing/fiji/sentinel_2/S2B_MSIL2A_20191023T220919_T01KBA/',
+                            'common_sensing/fiji/sentinel_2/S2B_MSIL2A_20191023T220919_T01KBB/']
 
 
 @mock_s3
-def test_get_measurements():
+def test_get_products_from_acquisition():
     s3 = S3(key=None, secret=None, s3_endpoint=None, region_name='us-east-1')
     initialise_cs_bucket(s3_resource=s3.s3_resource, bucket_name=BUCKET)
 
     repo = repository.S3Repository(s3)
-    measurements = repo.get_measurements(bucket=BUCKET, dataset_prefix='common_sensing/fiji/sentinel_2/'
-                                                                       'S2A_MSIL2A_20151022T222102_T01KBU/')
-
-    assert measurements == ['S2A_MSIL2A_20151022T222102_T01KBU_AOT_10m', 'S2A_MSIL2A_20151022T222102_T01KBU_B01_60m',
-                            'S2A_MSIL2A_20151022T222102_T01KBU_B02_10m', 'S2A_MSIL2A_20151022T222102_T01KBU_B03_10m',
-                            'S2A_MSIL2A_20151022T222102_T01KBU_B04_10m', 'S2A_MSIL2A_20151022T222102_T01KBU_B05_20m',
-                            'S2A_MSIL2A_20151022T222102_T01KBU_B06_20m', 'S2A_MSIL2A_20151022T222102_T01KBU_B07_20m',
-                            'S2A_MSIL2A_20151022T222102_T01KBU_B08_10m', 'S2A_MSIL2A_20151022T222102_T01KBU_B09_60m',
-                            'S2A_MSIL2A_20151022T222102_T01KBU_B11_20m', 'S2A_MSIL2A_20151022T222102_T01KBU_B12_20m',
-                            'S2A_MSIL2A_20151022T222102_T01KBU_B8A_20m', 'S2A_MSIL2A_20151022T222102_T01KBU_SCL_20m',
-                            'S2A_MSIL2A_20151022T222102_T01KBU_WVP_10m']
-
-
-@mock_s3
-def test_get_smallest_measurement_sample():
-    s3 = S3(key=None, secret=None, s3_endpoint=None, region_name='us-east-1')
-    initialise_cs_bucket(s3_resource=s3.s3_resource, bucket_name=BUCKET)
-
-    repo = repository.S3Repository(s3)
-    measurement = repo.get_smallest_measurement_sample(bucket=BUCKET,
-                                                       dataset_prefix='common_sensing/fiji/sentinel_2/'
+    products = repo.get_products_from_acquisition(bucket=BUCKET,
+                                                  acquisitions_prefix='common_sensing/fiji/sentinel_2/'
                                                                       'S2A_MSIL2A_20151022T222102_T01KBU/')
+
+    assert products == ['S2A_MSIL2A_20151022T222102_T01KBU_AOT_10m', 'S2A_MSIL2A_20151022T222102_T01KBU_B01_60m',
+                        'S2A_MSIL2A_20151022T222102_T01KBU_B02_10m', 'S2A_MSIL2A_20151022T222102_T01KBU_B03_10m',
+                        'S2A_MSIL2A_20151022T222102_T01KBU_B04_10m', 'S2A_MSIL2A_20151022T222102_T01KBU_B05_20m',
+                        'S2A_MSIL2A_20151022T222102_T01KBU_B06_20m', 'S2A_MSIL2A_20151022T222102_T01KBU_B07_20m',
+                        'S2A_MSIL2A_20151022T222102_T01KBU_B08_10m', 'S2A_MSIL2A_20151022T222102_T01KBU_B09_60m',
+                        'S2A_MSIL2A_20151022T222102_T01KBU_B11_20m', 'S2A_MSIL2A_20151022T222102_T01KBU_B12_20m',
+                        'S2A_MSIL2A_20151022T222102_T01KBU_B8A_20m', 'S2A_MSIL2A_20151022T222102_T01KBU_SCL_20m',
+                        'S2A_MSIL2A_20151022T222102_T01KBU_WVP_10m']
+
+
+@mock_s3
+def test_get_smallest_product_sample():
+    s3 = S3(key=None, secret=None, s3_endpoint=None, region_name='us-east-1')
+    initialise_cs_bucket(s3_resource=s3.s3_resource, bucket_name=BUCKET)
+
+    repo = repository.S3Repository(s3)
+    product = repo.get_smallest_product_sample(bucket=BUCKET,
+                                               acquisitions_prefix='common_sensing/fiji/sentinel_2/'
+                                                                   'S2A_MSIL2A_20151022T222102_T01KBU/')
 
     file = 'tests/data/sentinel_2/S2A_MSIL2A_20151022T222102_T01KBU/S2A_MSIL2A_20151022T222102_T01KBU_B02_10m.tif'
 
@@ -63,4 +64,4 @@ def test_get_smallest_measurement_sample():
     with open(file, "rb") as r:
         raster = r.read()
 
-    assert measurement == raster
+    assert product == raster
